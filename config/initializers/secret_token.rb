27e46b35ae580a27271711aaa80b8963b5a30f90
Friedrich Hehl <f@hehl.se>
2011-10-13 17:18:17 +0200

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuggestMe::Application.config.secret_token = 'c3d7a67cb722498030d3a105ba1657aaf47418557ba6ba4491c6bfb618463c0ae097a438938eb06af1ceaadec54bec403e932d27aff3a0df7c691998fed6c3fc'
